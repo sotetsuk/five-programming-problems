8027e852c79bf5fb055af90c4b20e7f7a63ce389
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-04-29 17:28:58 +0900

    fix prob4

--- prob4th.py
+++ prob4th.py
@@ -5,23 +5,21 @@ https://www.shiftedup.com/2015/05/07/five-programming-problems-every-software-en
 start 11:54
 end   12:02
 = 8min
+
+After I solved all problems, I found that the greedy solution is wrong.
+So I fixed using permutations and it took almost 10min.
 """
+
+from itertools import permutations
 
 
 def largest_num(l):
-    """
-    We can use greedy method
+    m = -1
+    for e in permutations(l):
+        m = max(int("".join([str(x) for x in e])), m)
 
-    :param l:
-    :return:
-    """
-
-    l_str = [str(e) for e in l]
-    l_str.sort()
-    ret = "".join(l_str[::-1])
-
-    return int(ret)
-
+    return m
 
 if __name__ == '__main__':
     print largest_num([50, 2, 1, 9])
+    print largest_num([50, 5, 56])
